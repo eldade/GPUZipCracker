47de96b51895f700b2d617d72265f144ceb51ce8
Eldad Eilam <eldade@me.com>
2017-10-10 14:51:01 -0700

    Initial Commit

---
+++ zipcracker/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  zipcracker
+//
+//  Created by Eldad Eilam on 10/10/17.
+//  Copyright © 2017 Eldad Eilam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
